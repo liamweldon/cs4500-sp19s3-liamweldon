9d78a3673c22261627b15d84e695eb46a8b1deaf
Liam Weldon <liam.liamweldon.weldon@gmail.com>
2019-01-23 20:47:23 -0500

    Added findUserById method to UserService

--- src/main/java/com/example/cs4500sp19s3lweldon/services/UserService.java
+++ src/main/java/com/example/cs4500sp19s3lweldon/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,5 +19,15 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return users;
+	}
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
